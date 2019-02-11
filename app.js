3016678e3d17438d9780bcb0b9a88470799c7784
codexempire <princewillifeanyi1999@gmail.com>
2019-02-11 23:49:53 +0100

    #163855782 setting up API environ

---
+++ app.js
@@ -0,0 +1,28 @@
+// importing dependencies
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+// creating a port where the app runs
+const port = process.env.port || 3000;
+
+// initial express
+const app = express();
+
+// use morgan
+app.use(morgan('tiny'));
+
+// use body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+// listen on the port
+app.listen(port, (err) => {
+    if (err) console.log(`Failed to run error: ${err}`);
+    console.log(`Running on ${port}`);
+});
+
+// export the app
+export default app;
